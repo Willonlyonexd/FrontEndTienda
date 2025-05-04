358ca9ee709687cdce044204715fbcb7a9dcaaa8
Junior Zamo <junior.zf.99@gmail.com>
2025-05-03 21:17:48 -0400

    terminado con backend deplegado

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,5 +1,5 @@
 export const GLOBAL={
-    url:'http://localhost:3000',
+     url: 'https://backendecommer-production.up.railway.app',
     // url:'https://ecommerce-backend-production-bceb.up.railway.app',
      cliente_id:'Aat1GXTAFWbFCeoLvA1UdTnSM30Qew6V5Iu9_cV0ynQJApoY18Lsirfm-SaoA8kHOVeft7NiFz8VNqw_'
 }
